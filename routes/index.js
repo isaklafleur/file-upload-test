b52268364fb5e4d990fa28ee4bcbd97288a18c9b
Isak La Fleur <isak.engdahl@gmail.com>
2017-11-16 11:01:41 +0100

    added multer file upload

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const multer = require("multer");
+
+const Picture = require("../models/pictures");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", (req, res, next) => {
+  res.render("index", { title: "Express" });
 });
+
+/* POST FILE WITH MULTER */;
+
+const upload = multer({ dest: "./public/uploads/" });
+
+router
+  .post("/upload", upload.single("photo"), (req, res, next) => {
+    const pic = new Picture({
+      name: req.body.name,
+      pic_path: `/uploads/${req.file.filename}`,
+      pic_name: req.file.originalname
+    });
+
+    pic.save((err) => {
+      if (err) {
+        next(err);
+      } else {
+        res.redirect("/");
+      }
+    });
+  });
+;
 
 module.exports = router;
